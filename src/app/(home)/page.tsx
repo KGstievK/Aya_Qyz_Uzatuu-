17507d493f4464e1d5cd479a57fba690f760e77b
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-10 02:28:12 +0600

    update4

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,13 @@
-"use client";
 
-import Home from "@/components/pages/Home";
-import React from "react";
+import Home from '@/components/pages/Home'
+import React from 'react'
 
 const page = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
 
-    return (
-      <div>
-        <Home/>
-      </div>
-      )
-   
-};  
-
-export default page;
+export default page
