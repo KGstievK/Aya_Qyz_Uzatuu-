9852737e2b256c7963107b0550e8c001a0c06ad4
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-10 02:37:41 +0600

    love

---
+++ src/components/pages/Dad/Dad.tsx
@@ -0,0 +1,17 @@
+import scss from './Dad.module.scss'
+
+
+const Dad = () => {
+  return (
+    <section className={scss.Dad}>
+      <div className="container">
+        <div className={scss.content}>
+          <p>ТЕРЕҢ УРМАТТОО МЕНЕН, ТОЙ ЭЭСИ:</p>
+          <h2>Арген</h2>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Dad
