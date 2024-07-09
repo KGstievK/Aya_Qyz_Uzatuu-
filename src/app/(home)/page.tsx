a829c836a2033448bb8e69963d5d917949b448b6
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-10 01:04:51 +0600

    fixo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,11 +3,10 @@
 import Home from "@/components/pages/Home";
 import Welcome from "@/components/pages/Welcome/Welcome";
 import React from "react";
-import { usePathname } from "next/navigation";
 
 const page = () => {
   const [animation, setAnimation] = React.useState(false);
-  
+
     return (
       <div>
         <Welcome setAnimation={setAnimation} />
