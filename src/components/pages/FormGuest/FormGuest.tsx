9038e382a2651d92cd1b9d3bf288dce12bede024
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-07-10 18:30:52 +0600

    InShaAllah

--- src/components/pages/FormGuest/FormGuest.tsx
+++ src/components/pages/FormGuest/FormGuest.tsx
@@ -1,39 +1,36 @@
-"use client"
-import { SubmitHandler, useForm } from 'react-hook-form'
-import scss from './FormGuest.module.scss'
-import axios from 'axios'
-import { useEffect } from 'react'
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import scss from "./FormGuest.module.scss";
+import axios from "axios";
 
 interface FormType {
-  _id?: number,
-  name?: string,
-  dev: string,
-  partner?: string,
-  comment?: string,
+  _id?: number;
+  name?: string;
+  dev: string;
+  partner?: string;
+  comment?: string;
 }
 
-const url = process.env.NEXT_PUBLIC_API_URL
+const url = process.env.NEXT_PUBLIC_API_URL;
 
 const FormGuest = () => {
-
-  const { register, handleSubmit } = useForm<FormType>({})
+  const { register, handleSubmit } = useForm<FormType>({});
   const onSubmit: SubmitHandler<FormType> = async (FormData) => {
     const guestData = {
       name: FormData.name,
       dev: FormData.dev,
       partner: FormData.partner,
       comment: FormData.comment,
-    }
-    const {data} = await axios.post(`${url}/wedding_v1`, guestData, {
+    };
+    const { data } = await axios.post(`${url}/wedding_v1`, guestData, {
       headers: {
         Accept: "application/json, text/plain, */*",
-        'Content-Type': "application/json"
-      }
-    })
-    window.location.reload()
-    console.log(data)
-  }
- 
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.reload();
+    console.log(data);
+  };
 
   return (
     <section className={scss.FormGuest}>
@@ -42,24 +39,44 @@ const FormGuest = () => {
           <h1>КОНОКТУН АНКЕТАСЫ</h1>
           <p>Сураныч, бир нече суроолорго жооп берип, катышууңузду ырастаңыз</p>
           <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder='Сиздин аты-жөнүңүс' {...register("name", {required: true})} />
+            <input
+              type="text"
+              placeholder="Сиздин аты-жөнүңүс"
+              {...register("name", { required: true })}
+            />
             <p>ТАКТОО</p>
             <div className={scss.radio}>
-              <input  type="radio" value="Приду✅" {...register("dev", {required: true})} />
+              <input
+                type="radio"
+                value="Приду✅"
+                {...register("dev", { required: true })}
+              />
               <p>КЕЛЕМ</p>
             </div>
             <div className={scss.radio}>
-              <input  type="radio" value="Не смогу❌" {...register("dev", {required: true})}/>
+              <input
+                type="radio"
+                value="Не смогу❌"
+                {...register("dev", { required: true })}
+              />
               <p>КЕЛЕ АЛБАЙМ</p>
             </div>
-            <input type="text" placeholder='Сиз ким менен келесиз, аты-жөнүн жазып коюңуз' {...register("partner", {required: true})} />
-            <input type="text" placeholder='Эгер суроолоруңуз болсо жазып коюңуз' {...register("comment", {required: true})} />
-            <button type='submit'>ЖӨНӨТҮҮ</button>
+            <input
+              type="text"
+              placeholder="Сиз ким менен келесиз, аты-жөнүн жазып коюңуз"
+              {...register("partner", { required: false })}
+            />
+            <input
+              type="text"
+              placeholder="Эгер суроолоруңуз болсо жазып коюңуз"
+              {...register("comment", { required: false })}
+            />
+            <button type="submit">ЖӨНӨТҮҮ</button>
           </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FormGuest
+export default FormGuest;
